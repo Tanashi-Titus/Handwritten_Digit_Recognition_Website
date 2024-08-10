4d49aba2f03dea7c1e7abf8f04e711817757117d
Tanashi <giap.titus@gmail.com>
2024-08-10 22:53:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -59,5 +59,4 @@ if __name__ == "__main__":
     import os
     assert os.path.exists(SAVE_MODEL_PATH), "no saved model"
     predict = Predict()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
