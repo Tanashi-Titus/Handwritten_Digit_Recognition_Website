7b628e5c819973da5e87d56a5a92e91bd83bbd2f
Tanashi <giap.titus@gmail.com>
2024-08-11 17:13:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,36 @@
 import json
+
+import numpy as np
 import torch
 from flask import Flask, render_template, request
 from PIL import Image, ImageChops, ImageOps
 from torchvision import transforms
+
 from model import Model
 from train import SAVE_MODEL_PATH
 
 app = Flask(__name__)
 predict = None
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/DigitRecognition", methods=["POST"])
 def predict_digit():
     img = Image.open(request.files["img"]).convert("L")
 
     # predict
-    res_json = {"pred": 0, "probs": [0]}
+    res_json = {"pred": "Err", "probs": []}
     if predict is not None:
         res = predict(img)
-        res_json["pred"] = int(res.argmax())
+        res_json["pred"] = str(np.argmax(res))
         res_json["probs"] = [p * 100 for p in res]
 
     return json.dumps(res_json)
+
 
 class Predict():
     def __init__(self):
@@ -34,14 +40,25 @@ class Predict():
         self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 
     def _centering_img(self, img):
-        left, top, right, bottom = img.getbbox()
         w, h = img.size[:2]
-        shift_x = (left + (right - left) // 2) - w // 2
-        shift_y = (top + (bottom - top) // 2) - h // 2
-        return ImageChops.offset(img, -shift_x, -shift_y)
+        left, top, right, bottom = w, h, -1, -1
+        imgpix = img.getdata()
+
+        for y in range(h):
+            yoffset = y * w
+            for x in range(w):
+                if imgpix[yoffset + x] > 0:
+                    left = min(left, x)
+                    top = min(top, y)
+                    right = max(right, x)
+                    bottom = max(bottom, y)
+
+        shiftX = (left + (right - left) // 2) - w // 2
+        shiftY = (top + (bottom - top) // 2) - h // 2
+        return ImageChops.offset(img, -shiftX, -shiftY)
 
     def __call__(self, img):
-        img = ImageOps.invert(img)
+        img = ImageOps.invert(img)  # MNIST image is inverted
         img = self._centering_img(img)
         img = img.resize((28, 28), Image.BICUBIC)  # resize to 28x28
         tensor = self.transform(img)
@@ -59,4 +76,5 @@ if __name__ == "__main__":
     import os
     assert os.path.exists(SAVE_MODEL_PATH), "no saved model"
     predict = Predict()
-    app.run(host="0.0.0.0", port=5000)
+
+    app.run(host="0.0.0.0")
